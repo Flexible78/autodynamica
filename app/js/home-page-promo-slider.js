94de3712667f2f7c17649ea0f6a9a18026919d37
Alexander <alexandertsyrkin@gmail.com>
2023-08-01 01:26:55 +0400

    feat: fix promo wiper

--- app/js/home-page-promo-slider.js
+++ app/js/home-page-promo-slider.js
@@ -1,14 +1,11 @@
-const swiper = new Swiper('.swiper', {
-   // Optional parameters
+const promoSlider = new Swiper('.swiper', {
    direction: 'horizontal',
    loop: true,
 
-   // If we need pagination
    pagination: {
       el: '.swiper-pagination',
    },
 
-   // Navigation arrows
    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
