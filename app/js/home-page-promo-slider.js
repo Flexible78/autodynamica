7de16470332d051a42dff23bab70449a70795f2c
Alexander <alexandertsyrkin@gmail.com>
2023-08-02 11:14:04 +0400

    feat: fix promo-slider

--- app/js/home-page-promo-slider.js
+++ app/js/home-page-promo-slider.js
@@ -1,20 +1,12 @@
 const promoSwiper = new Swiper('.swiper', {
    direction: 'horizontal',
    loop: true,
-   speed: 500,
-   keyboard: true,
-   effect: 'flip', // slide, flip,fade
-   initialSlide: 1,
-   slidesPerView: 1,
-   slidesPerGroup: 1,
-   freeMode: false,
    pagination: {
       el: '.swiper-pagination',
-      clickable: true,
    },
 
-   autoplay: {
-      delay: 2000,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
    },
-
 });
