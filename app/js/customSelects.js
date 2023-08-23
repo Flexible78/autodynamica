09785ddf6afb395781707a1d336a274198e054ea
Alexander <alexandertsyrkin@gmail.com>
2023-08-23 12:06:13 +0400

    feat:fix custom-select

--- app/js/customSelects.js
+++ app/js/customSelects.js
@@ -32,12 +32,8 @@ const optionAttributes = {
    'aria-selected': false,
    'data-value': '',
 };
-function openSelect(elem) {
-   elem.classList.add('open');
-}
-function closeSelect(elem) {
-   elem.classList.remove('open');
-}
+
+/* Execute */
 document.addEventListener('DOMContentLoaded', () => {
    const defaultSelects = findElements('select');
    hideElement(defaultSelects);
@@ -48,22 +44,63 @@ document.addEventListener('DOMContentLoaded', () => {
    customSelects.forEach((select) => {
       select.addEventListener('click', function (event) {
          const { target } = event;
-         console.log(target);
-         console.log(this);
+
          if (target.classList.contains('select__trigger') && !this.classList.contains('open')) {
-            openSelect(this);
+            openSelect(this, '.select__trigger');
          } else {
-            closeSelect(this);
+            closeSelect(this, '.select__trigger');
+         }
+
+         if (target.classList.contains('select__option')) {
+            pickOption(this, target);
          }
       });
    });
 });
 
 /* Functions */
+function pickOption(select, selectedItem) {
+   const trigger = select.querySelector('.select__trigger');
+   const triggerText = trigger.querySelector('.select__trigger-text');
+   const optionsList = select.querySelectorAll('.select__option');
+   const defaultSelects = select.querySelector('select');
+   const defaultOptions = defaultSelects.querySelectorAll('option');
+
+   triggerText.innerText = selectedItem.innerText;
+
+   optionsList.forEach((el) => {
+      el.setAttribute('aria-selected', false);
+   });
+
+   selectedItem.setAttribute('aria-selected', true);
+   trigger.setAttribute('aria-activedescendant', `${selectedItem.id}`);
+
+   const selectedOptionValue = selectedItem.dataset.value;
+   defaultOptions.forEach((option) => {
+      if (option.value === selectedOptionValue) {
+         option.selected = true;
+      }
+   });
+
+   optionsList.forEach((el) => {
+      el.classList.remove('selected');
+   });
+   selectedItem.classList.add('selected');
+}
+function openSelect(elem, find) {
+   elem.classList.add('open');
+   const trigger = elem.querySelector(find);
+   trigger.setAttribute('aria-expanded', true);
+}
+function closeSelect(elem. find) {
+   elem.classList.remove('open');
+   const trigger = elem.querySelector(find);
+   trigger.setAttribute('aria-expanded', false);
+}
+
 function renderSelect(defaultSelects) {
    defaultSelects.forEach((select) => {
       const customSelect = createNode('div', customSelectAttributes, select.parentNode);
-      customSelect.classList.add('open'); // TODO:
       customSelect.appendChild(select);
 
       const selectTrigger = createNode('div', triggerAttributes, customSelect);
@@ -119,35 +156,3 @@ function findElements(element) {
    }
    return document.querySelectorAll(element);
 }
-
-/*
-'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-   findElements('select');
-});
-
-/!* Functions *!/
-
-function hideElement(collection) {
-   if (collection == null) {
-      return;
-   }
-   collection.forEach((currentElement) => {
-      if (currentElement.tagName.toLowerCase() === 'select') {
-         currentElement.classList.add('hidden');
-         currentElement.setAttribute('tabindex', -1);
-         currentElement.setAttribute('aria-hidden', true);
-      } else {
-         currentElement.classList.add('visually-hidden');
-         currentElement.setAttribute('aria-hidden', true);
-      }
-   });
-}
-function findElements(element) {
-   if (document.querySelectorAll(element) == null) {
-      return;
-   }
-   return document.querySelectorAll(element);
-}
-*/
--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -32,12 +32,7 @@ const optionAttributes = {
    'aria-selected': false,
    'data-value': '',
 };
-function openSelect(elem) {
-   elem.classList.add('open');
-}
-function closeSelect(elem) {
-   elem.classList.remove('open');
-}
+
 document.addEventListener('DOMContentLoaded', () => {
    const defaultSelects = findElements('select');
    hideElement(defaultSelects);
@@ -55,11 +50,49 @@ document.addEventListener('DOMContentLoaded', () => {
          } else {
             closeSelect(this);
          }
+         if (target.classList.contains('select__option')) {
+            pickOption(this, target);
+         }
       });
    });
 });
 
 /* Functions */
+function pickOption(select, selectedItem) {
+   const trigger = select.querySelector('.select__trigger');
+   const triggerText = trigger.querySelector('.select__trigger-text');
+   const optionsList = select.querySelectorAll('.select__option');
+   const defaultSelects = select.querySelector('select');
+   const defaultOptions = defaultSelects.querySelectorAll('option');
+
+   triggerText.innerText = selectedItem.innerText;
+
+   optionsList.forEach((el) => {
+      el.setAttribute('aria-selected', false);
+   });
+
+   selectedItem.setAttribute('aria-selected', true);
+   trigger.setAttribute('aria-activedescendant', `${selectedItem.id}`);
+
+   const selectedOptionValue = selectedItem.dataset.value;
+   defaultOptions.forEach((option) => {
+      if (option.value === selectedOptionValue) {
+         option.selected = true;
+      }
+   });
+
+   optionsList.forEach((el) => {
+      el.classList.remove('selected');
+   });
+   selectedItem.classList.add('selected');
+}
+function openSelect(elem) {
+   elem.classList.add('open');
+}
+function closeSelect(elem) {
+   elem.classList.remove('open');
+}
+
 function renderSelect(defaultSelects) {
    defaultSelects.forEach((select) => {
       const customSelect = createNode('div', customSelectAttributes, select.parentNode);
@@ -119,35 +152,3 @@ function findElements(element) {
    }
    return document.querySelectorAll(element);
 }
-
-/*
-'use strict';
-
-document.addEventListener('DOMContentLoaded', () => {
-   findElements('select');
-});
-
-/!* Functions *!/
-
-function hideElement(collection) {
-   if (collection == null) {
-      return;
-   }
-   collection.forEach((currentElement) => {
-      if (currentElement.tagName.toLowerCase() === 'select') {
-         currentElement.classList.add('hidden');
-         currentElement.setAttribute('tabindex', -1);
-         currentElement.setAttribute('aria-hidden', true);
-      } else {
-         currentElement.classList.add('visually-hidden');
-         currentElement.setAttribute('aria-hidden', true);
-      }
-   });
-}
-function findElements(element) {
-   if (document.querySelectorAll(element) == null) {
-      return;
-   }
-   return document.querySelectorAll(element);
-}
-*/
